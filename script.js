d7e253bb0d283d7182dcb2265e0c8b921983a750
JoanChirinos <jchirinos@stuy.edu>
2018-09-20 20:19:24 -0400

    .

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function pop_lessons() {
     if (!showing_lessons) {
         l = "<div class=\"row\">\n<div class=\"col\"><ul>\n";
         for (var i = 0; i < lessons.length; i++) {
-            l += "<li><a target=\"_blank\"href=\"/Lessons/" + lessons[i][1] + "\">" + lessons[i][0] + "</a></li>\n";
+            l += "<li><a target=\"_blank\"href=\"Lessons/" + lessons[i][1] + "\">" + lessons[i][0] + "</a></li>\n";
         }
         l += "</ul></div></div>\n";
         showing_lessons = true;
