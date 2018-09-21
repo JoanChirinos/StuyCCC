b76d7935e6c6ab5994235203f5916aae100e3c59
JoanChirinos <jchirinos@stuy.edu>
2018-09-20 21:49:54 -0400

    added interest form

---
+++ Interest/form.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python3
+
+import cgi, cgitb
+cgitb.enable()
+
+
+def go():
+    print('Content-type: text/html\n\n')
+    fs = cgi.FieldStorage()
+    
+    fname = fs.getvalue('firstName', 'Joan')
+    lname = fs.getvalue('lastName', 'Chirinos')
+    email = fs.getvalue('email', 'jchirinos@stuy.edu')
+    
+    f = open('people.txt', 'a')
+    f.write(','.join([fname, lname, email]))
+    f.close()
+    print("""<html><head></head><body><script>window.onload = function() {
+    // similar behavior as clicking on a link
+    window.location.href = "http://homer.stuy.edu/~jchirinos/StuyCCC/Interest/index.html";
+}</script></body></html>""")
+
+go()
