2209e53646b5763bac119cdfdd5b3daa5e841f67
JoanChirinos <jchirinos@stuy.edu>
2018-09-21 11:51:44 -0400

    updated site

--- script.js
+++ script.js
@@ -16,40 +16,49 @@ var showing_utils = false;
 var showing_links = false;
 
 function pop_lessons() {
+    console.log('\n\n=====  populating lessons =====\n');
     var l = ""
     if (!showing_lessons) {
         l = "<div class=\"row\">\n<div class=\"col\"><ul>\n";
         for (var i = 0; i < lessons.length; i++) {
             l += "<li><a target=\"_blank\"href=\"Lessons/" + lessons[i][1] + "\">" + lessons[i][0] + "</a></li>\n";
+            console.log("<li><a target=\"_blank\"href=\"Lessons/" + lessons[i][1] + "\">" + lessons[i][0] + "</a></li>\n");
         }
         l += "</ul></div></div>\n";
         showing_lessons = true;
     } else showing_lessons = false;
     document.getElementById("lessons").innerHTML = l;
+    console.log('===== done populating lessons =====');
 }
 
 function pop_utils() {
+    console.log('\n\n=====  populating utils =====\n');
     var l = ""
     if (!showing_utils) {
         l = "<div class=\"row\">\n<div class=\"col\"><ul>\n";
         for (var i = 0; i < utils.length; i++) {
             l += "<li><a target=\"_blank\"href=\"Utils/" + utils[i][1] + "\">" + utils[i][0] + "</a></li>\n";
+            console.log("<li><a target=\"_blank\"href=\"Utils/" + utils[i][1] + "\">" + utils[i][0] + "</a></li>\n");
         }
         l += "</ul></div></div>\n";
         showing_utils = true;
     } else showing_utils = false;
     document.getElementById("utils").innerHTML = l;
+    console.log('===== done populating utils =====');
 }
 
 function pop_links() {
+    console.log('\n\n=====  populating links =====\n');
     var l = ""
     if (!showing_links) {
         l = "<div class=\"row\">\n<div class=\"col\"><ul>\n";
         for (var i = 0; i < links.length; i++) {
             l += "<li><a target=\"_blank\"href=\"" + links[i][1] + "\">" + links[i][0] + "</a></li>\n";
+            console.log("<li><a target=\"_blank\"href=\"" + links[i][1] + "\">" + links[i][0] + "</a></li>\n");
         }
         l += "</ul></div></div>\n";
         showing_links = true;
     } else showing_links = false;
     document.getElementById("links").innerHTML = l;
+    console.log('===== done populating links =====');
 }
