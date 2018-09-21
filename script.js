b34caa0600aa0078e247fea7b8c244cff9ba7359
JoanChirinos <jchirinos@stuy.edu>
2018-09-21 12:21:32 -0400

    updated site

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ function pop_leadership() {
     console.log('\n\n=====  populating leadership =====\n');
     var l = ""
     if (!showing_leadership) {
-        l = "<div class=\"row\">\n<div class=\"col-sm\"><center>\n";
+        l = "<div class=\"row\">\n<div class=\"col\"><center>\n";
         for (var i = 0; i < leadership.length / 2; i += 2) {
             var p = leadership[i];
             l += "<h3>" + p[0] + '</h3>\n';
@@ -94,7 +94,7 @@ function pop_leadership() {
             }
             l += "<p><i>" + p[1] + " | " + p[2] + "</i></p>\n"
         }
-        l += "</center></div>\n<div class=\"col-sm\"><center>";
+        l += "</center></div>\n<div class=\"col\"><center>";
         for (var i = 1; i < leadership.length; i += 2) {
             var p = leadership[i];
             l += "<h3>" + p[0] + '</h3>\n';
