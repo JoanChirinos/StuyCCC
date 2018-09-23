b4c7072b70b3d00bdc200d8c600bd45b286bbf03
JoanChirinos <jchirinos@stuy.edu>
2018-09-23 18:21:50 -0400

    updated site

---
+++ Forms/Shirts/shirtform.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python3
+
+import cgi, cgitb
+cgitb.enable()
+
+
+def go():
+    fs = cgi.FieldStorage()
+    
+    #get values
+    shirt = fs.getvalue('shirt', '')
+    
+    #get old values
+    straw = open('shirts.txt', 'r')
+    text = straw.read()
+    straw.close()
+    
+    #format text
+    vals = [int(text.split('\n')[0]), int(text.split('\n')[1])]
+    
+    if shirt != '':
+        if shirt == '1':
+            vals[0]++
+        elif shirt == '2':
+            vals[1]++
+    
+    #write new vals
+    straw = open('shirts.txt', 'w')
+    straw.write('\n'.join(str(i) for i in vals))
+    straw.close()
+    
+    #redirect
+    print("""<html><head></head><body><script>window.onload = function() {
+    window.location.href = "http://homer.stuy.edu/~jchirinos/StuyCCC/";
+    }</script></body></html>""")
+    
+go()
