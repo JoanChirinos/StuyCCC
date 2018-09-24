a2fdc91a0809459b512d659c419682c315611eaa
JoanChirinos <jchirinos@stuy.edu>
2018-09-24 12:30:04 -0400

    updated site

--- Forms/Shirts/script.js
+++ Forms/Shirts/script.js
@@ -1,4 +1,23 @@
+/*
 function load_images() {
     document.getElementById('img1').innerHTML = '<img src="/~jchirinos/StuyCCC/data/imgs/Shirt1.png"><br>';
-    document.getElementById('img2').innerHTML = '<img src="/~jchirinos/StuyCCC/data/imgs/Shirt2.png"><br>'
+    document.getElementById('img2').innerHTML = '<img src="/~jchirinos/StuyCCC/data/imgs/Shirt2.png"><br>';
 }
+*/
+
+var img1 = document.getElementById('img1');
+var img2 = document.getElementById('img2');
+
+var dwn_img1 = new Image();
+var dwn_img2 = new Image();
+
+dwn_img1.onload = function () {
+    img1.src = this.src;
+};
+
+dwn_img2.onload = function () {
+    img2.src = this.src;
+};
+
+dwn_img1.src = "/~jchirinos/StuyCCC/data/imgs/Shirt1.png";
+dwn_img1.src = "/~jchirinos/StuyCCC/data/imgs/Shirt1.png";
