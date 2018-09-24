e0265c545c1c22630cf97f3427373be0de953c7c
JoanChirinos <jchirinos@stuy.edu>
2018-09-24 12:21:06 -0400

    updated site

---
+++ Forms/Shirts/script.js
@@ -0,0 +1,4 @@
+function load_images() {
+    document.getElementById('img1').innerHTML = '<img src="/~jchirinos/StuyCCC/data/imgs/Shirt1.png"><br>';
+    document.getElementById('img2').innerHTML = '<img src="/~jchirinos/StuyCCC/data/imgs/Shirt2.png"><br>'
+}
