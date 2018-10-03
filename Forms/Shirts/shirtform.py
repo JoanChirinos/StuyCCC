45508586472c185c3c210c21979749cb43b69d15
JoanChirinos <jchirinos@stuy.edu>
2018-10-02 23:50:48 -0400

    fixed py

--- Forms/Shirts/shirtform.py
+++ Forms/Shirts/shirtform.py
@@ -17,20 +17,22 @@ def go():
     straw.close()
     
     #format text
-    vals = [int(text.split('\n')[0]), int(text.split('\n')[1])]
+    vals = [int(text.split('\n')[0]), int(text.split('\n')[1]), int(text.split('\n')[2])]
     
     if shirt != '':
         if shirt == '1':
             vals[0] += 1
         elif shirt == '2':
             vals[1] += 1
+        elif shirt == '3':
+            vals[2] += 1
     
     #write new vals
     straw = open('shirts.txt', 'w')
     straw.write('\n'.join(str(i) for i in vals))
     straw.close()
     
-    #redirect
+    #close tab
     print("""<html><head></head><body><script>window.onload = function() {
     window.close();
     }</script></body></html>""")
